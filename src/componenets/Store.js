0907845daa094c4adfad0bfbe886f8668dbd34fc
DaniilKutyavin <daniilkkutyavin@yandex.ru>
2024-11-26 02:27:56 +0500

    asfasfdsdsgfsa

--- src/componenets/Store.js
+++ src/componenets/Store.js
@@ -23,7 +23,31 @@ const Store = () => {
 
         {/* Слоган в правом верхнем углу */}
         <div className="acid-slogan ttop-right">
-          <img src={sloganSvg} alt="Slogan" />
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 500 500"
+            className="slogan-svg"
+          >
+            <style type="text/css">
+              {`.st0 { opacity: 0.46; }
+                .st1 { fill: #F2F2F2; }
+                .st2 { font-family: 'Impact'; }
+                .st3 { font-size: 86.6175px; }`}
+            </style>
+            <g className="st0">
+              <text transform="matrix(1 0 0 1.0368 41.0332 179.4746)">
+                <tspan x="0" y="0" className="st1 st2 st3">
+                  ТЕРРИТОРИЯ{' '}
+                </tspan>
+                <tspan x="-43" y="100.3" className="st1 st2 st3">
+                  СЧАСТЛИВОГО{' '}
+                </tspan>
+                <tspan x="49.9" y="200.5" className="st1 st2 st3">
+                  ФЕРМЕРА
+                </tspan>
+              </text>
+            </g>
+          </svg>
         </div>
       </div>
     </div>
